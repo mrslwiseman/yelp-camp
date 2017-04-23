1f86ebe13ea268b44b31df233e3d9a79db8966e2
Matt Wiseman <mrslwiseman@gmail.com>
2017-04-23 16:09:24 +1000

    tweaked some css

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require("express"),
- 		ejs = require("ejs"),
- 		bodyParser = require("body-parser"),
-		mongoose = require("mongoose"),
-    Campground = require('./models/campgrounds'),
-    Comment = require('./models/comment'),
-    seedDB = require('./seeds');
-    seedDB();
+  ejs = require("ejs"),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  Campground = require('./models/campgrounds'),
+  Comment = require('./models/comment'),
+  seedDB = require('./seeds');
+seedDB();
 
 let app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 // connect to mongodb
-mongoose.connect("mongodb://localhost:27017/yelp-camp-v2");
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 
 
 // set up routes
 
-app.get("/", (req,res) => {
-	res.render("home")
+app.get("/", (req, res) => {
+  res.render("home")
 })
 
 
@@ -30,41 +32,45 @@ app.get("/", (req,res) => {
 
 // RESTful conventions to keep .get and .post on same route
 // INDEX ROUTE - show all campgrounds
-app.get("/campgrounds", (req,res) => {
+app.get("/campgrounds", (req, res) => {
   // get all campgrounds from DB
-Campground.find({}, (err, allcampgrounds) => {
-  if(err) {return console.log(err)} else {
-    res.render("campgrounds/campgrounds", {campgrounds:allcampgrounds})
+  Campground.find({}, (err, allcampgrounds) => {
+    if (err) {
+      return console.log(err)
+    } else {
+      res.render("campgrounds/campgrounds", {
+        campgrounds: allcampgrounds
+      })
 
-  }
-})
+    }
+  })
 })
 //CREATE ROUTE - add new campground to DB
-app.post("/campgrounds", (req,res) => {
-	let name = req.body.name;
-	let image = req.body.image;
+app.post("/campgrounds", (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
   let description = req.body.description;
 
-Campground.create({
-  name: name,
-  image: image,
-  description: description
-}, (err,campground) => {
-  if(err) return console.log(err)
-  console.log("SUCCESS CAMPGROUND ADDED: " + campground)
-})
+  Campground.create({
+    name: name,
+    image: image,
+    description: description
+  }, (err, campground) => {
+    if (err) return console.log(err)
+    console.log("SUCCESS CAMPGROUND ADDED: " + campground)
+  })
 
   // campgrounds.push({name:name, image:image})
-	res.redirect("/campgrounds")
-	// get data from form and add to campgrounds array
-	// redirect back to campgrounds page
+  res.redirect("/campgrounds")
+  // get data from form and add to campgrounds array
+  // redirect back to campgrounds page
 
 }) // end POST route
 
 
 // NEW ROUTE - display form to add new campgrounds
-app.get("/campgrounds/new", (req,res) => {
-	res.render("campgrounds/new")
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new")
 })
 
 
@@ -72,21 +78,23 @@ app.get("/campgrounds/new", (req,res) => {
 
 
 // SHOW ROUTE - show page for id.
-app.get("/campgrounds/:id", (req,res) => {
+app.get("/campgrounds/:id", (req, res) => {
 
   // res.send("This will be the SHOW page for campground ID: " + id)
 
   // find the campground with supplied ID
-Campground.findById(req.params.id).populate("comments").exec( (err, foundCampground) => {
-       if(err) {
-          console.log(err)
-       } else {
+  Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+    if (err) {
+      console.log(err)
+    } else {
 
-    res.render("campgrounds/show", {campground: foundCampground})
-  }
+      res.render("campgrounds/show", {
+        campground: foundCampground
+      })
+    }
 
 
-})
+  })
 
 
 })
@@ -97,36 +105,36 @@ Campground.findById(req.params.id).populate("comments").exec( (err, foundCampgro
 // COMMENTS ROUTE
 // ============================
 // ADD COMMENT ROUTE
-app.get("/campgrounds/:id/comments/new", (req,res) => {
+app.get("/campgrounds/:id/comments/new", (req, res) => {
   Campground.findById(req.params.id, (err, campground) => {
-    if(err){
+    if (err) {
       console.log(err)
-      } else {
-        res.render("comments/new", {campground:campground});
+    } else {
+      res.render("comments/new", {
+        campground: campground
+      });
     }
   })
 
 })
 
-app.post("/campgrounds/:id/comments", (req,res) => {
-console.log(req.params.id)
-Campground.findById(req.params.id, (err, campground) => {
-  if(err) {
-    console.log(err)
-  } else {
-    Comment.create({
-      text : req.body.comment.text,
-      author: req.body.comment.author
-    }, (err, comment) => {
-      campground.comments.push(comment);
-      campground.save();
-      res.redirect("/campgrounds/" + req.params.id)
-    })
-  }
+app.post("/campgrounds/:id/comments", (req, res) => {
+  console.log(req.params.id)
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err)
+    } else {
+      Comment.create({
+        text: req.body.comment.text,
+        author: req.body.comment.author
+      }, (err, comment) => {
+        campground.comments.push(comment);
+        campground.save();
+        res.redirect("/campgrounds/" + req.params.id)
+      })
+    }
 
-})
-
-
+  })
 
 
 
@@ -143,5 +151,5 @@ Campground.findById(req.params.id, (err, campground) => {
 // set up listening
 
 app.listen(3000, () => {
-	console.log(`server started...http://localhost:3000/`)
+  console.log(`server started...http://localhost:3000/`)
 })
--- seeds.js
+++ seeds.js
@@ -5,27 +5,32 @@ let data = [
   {
     name: "Lakey Lake Camp",
     image: "https://farm7.staticflickr.com/6210/6090170567_6df55f8d83.jpg",
-    description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
+    description: "Activated charcoal kombucha yuccie, vegan neutra try-hard echo park ethical deep v. Biodiesel meditation literally prism typewriter church-key. Whatever prism mumblecore, marfa activated charcoal wayfarers trust fund vexillologist beard ugh quinoa kinfolk offal gochujang. Affogato drinking vinegar kickstarter, schlitz bushwick fap subway tile synth bespoke vape actually keffiyeh succulents. 3 wolf moon post-ironic selfies, raw denim food truck yr gluten-free. Raw denim prism fanny pack, gastropub keytar tumblr mixtape 3 wolf moon try-hard woke. Mustache 8-bit cronut, 3 wolf moon tumeric retro meditation leggings."
   },
   {
     name: "Golden Gums Camp",
     image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg",
-    description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
+    description: "Activated charcoal kombucha yuccie, vegan neutra try-hard echo park ethical deep v. Biodiesel meditation literally prism typewriter church-key. Whatever prism mumblecore, marfa activated charcoal wayfarers trust fund vexillologist beard ugh quinoa kinfolk offal gochujang. Affogato drinking vinegar kickstarter, schlitz bushwick fap subway tile synth bespoke vape actually keffiyeh succulents. 3 wolf moon post-ironic selfies, raw denim food truck yr gluten-free. Raw denim prism fanny pack, gastropub keytar tumblr mixtape 3 wolf moon try-hard woke. Mustache 8-bit cronut, 3 wolf moon tumeric retro meditation leggings."
   },
   {
     name: "Mosquito Bay",
     image: "https://farm6.staticflickr.com/5319/7407436246_0ac54dd559.jpg",
-    description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
+    description: "Activated charcoal kombucha yuccie, vegan neutra try-hard echo park ethical deep v. Biodiesel meditation literally prism typewriter church-key. Whatever prism mumblecore, marfa activated charcoal wayfarers trust fund vexillologist beard ugh quinoa kinfolk offal gochujang. Affogato drinking vinegar kickstarter, schlitz bushwick fap subway tile synth bespoke vape actually keffiyeh succulents. 3 wolf moon post-ironic selfies, raw denim food truck yr gluten-free. Raw denim prism fanny pack, gastropub keytar tumblr mixtape 3 wolf moon try-hard woke. Mustache 8-bit cronut, 3 wolf moon tumeric retro meditation leggings."
   },
   {
     name: "Tahini Reservoir Camp",
     image: "https://farm9.staticflickr.com/8023/7626458374_7ddea1aa2c.jpg",
-    description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
+    description: "Activated charcoal kombucha yuccie, vegan neutra try-hard echo park ethical deep v. Biodiesel meditation literally prism typewriter church-key. Whatever prism mumblecore, marfa activated charcoal wayfarers trust fund vexillologist beard ugh quinoa kinfolk offal gochujang. Affogato drinking vinegar kickstarter, schlitz bushwick fap subway tile synth bespoke vape actually keffiyeh succulents. 3 wolf moon post-ironic selfies, raw denim food truck yr gluten-free. Raw denim prism fanny pack, gastropub keytar tumblr mixtape 3 wolf moon try-hard woke. Mustache 8-bit cronut, 3 wolf moon tumeric retro meditation leggings."
   },
 ]
 
 function seedDB(){
   // remove all campgrounds
+  Comment.remove({}, (err) => {
+    if(err) {
+      console.log(err)
+    }
+  });
   Campground.remove({}, (err) => {
     if(err) {
       console.log(err)
@@ -40,18 +45,18 @@ function seedDB(){
         if (err) {console.log(err)}
         else {
           console.log(campground + " Campground added");
-          // Comment.create({
-          //   text: "Great campground, sandflies were TERRIBLE!",
-          //   author: "Bob"
-          // }, (err, comment) => {
-          //   if(err) {
-          //     console.log(err)
-          //   } else {
-          //     camp.comments.push(comment);
-          //     camp.save();
-          //     console.log("Comment added")
-          //   }
-          // })
+          Comment.create({
+            text: "Great campground, sandflies were TERRIBLE!",
+            author: "Bob"
+          }, (err, comment) => {
+            if(err) {
+              console.log(err)
+            } else {
+              camp.comments.push(comment);
+              camp.save();
+              console.log("Comment added")
+            }
+          })
 
 
         }
